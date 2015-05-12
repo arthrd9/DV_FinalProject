09c80cea914c5cdf558d461f12182328035bc47c
Madeline Packard <madelinepackard@utexas.edu>
2015-05-12 14:43:52 -0500

    need to update the .rmd insert png code

--- 01 Data/Dataframes.R
+++ 01 Data/Dataframes.R
@@ -1,9 +1,25 @@
+#Call datasets with appropriate SQL
 grad_earnings <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, GENDER, STATS, VALUE from GRAD_EARNINGS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-
-grad_earnings %>% select(GEO, SCHOOL, VALUE) %>% filter(SCHOOL %in% c("Doctorate")) %>% tbl_df
 
 grad_debt <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, SOURCE, STATS, VALUE from GRAD_DEBT "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 grad_loans <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, SOURCE, STATS, VALUE from GRAD_LOANS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 grad_employment <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, GENDER, STATS, VALUE from GRAD_EMPLOYMENT "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+#Data wrangling and PLOTS
+#Avg DOCTORATE Grad Earnings in each Province, Year, by Gender
+grad_earnings %>% filter(SCHOOL %in% c("Doctorate"), STATS == "Median", GENDER != "Both sexes", VALUE != "null") %>% ggplot(aes(x=GEO, y=VALUE, color=GENDER)) + geom_point() + ggtitle('Average Doctorate Graduate Earnings') + scale_color_brewer(palette="Set1") + facet_wrap(~REF_DATE) + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.4), plot.title = element_text(size=20, face="bold", vjust=2), axis.title.x = element_blank())
+
+#Create new table
+doctorate_earnings <- grad_earnings %>% filter(SCHOOL %in% c("Doctorate"), STATS == "Median", GENDER != "Both sexes", VALUE != "null")
+
+##Avg COLLEGE Grad Earnings in each Province, Year, by Gender
+grad_earnings %>% filter(SCHOOL %in% c("College"), STATS == "Median", GENDER != "Both sexes", VALUE != "null") %>% ggplot(aes(x=GEO, y=VALUE, color=GENDER)) + geom_point() + ggtitle('Average College Graduate Earnings') + scale_color_brewer(palette="Set1") + facet_wrap(~REF_DATE) + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.4), plot.title = element_text(size=20, face="bold", vjust=2), axis.title.x = element_blank())
+
+#Create new table
+college_earnings <- grad_earnings %>% filter(SCHOOL %in% c("College"), STATS == "Median", GENDER != "Both sexes", VALUE != "null")
+
+#To remove ticks and text on axis
+#axis.ticks.y = element_blank(),axis.text.y = element_blank()
+
