4eb9c4fd938d6a7e962604993fe3e693e489ca41
Madeline Packard <madelinepackard@utexas.edu>
2015-05-12 16:42:05 -0500

    need to update png code in rmd, finish loans

--- 01 Data/Dataframes.R
+++ 01 Data/Dataframes.R
@@ -8,18 +8,44 @@ grad_loans <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.
 grad_employment <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, GENDER, STATS, VALUE from GRAD_EMPLOYMENT "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 #Data wrangling and PLOTS
-#Avg DOCTORATE Grad Earnings in each Province, Year, by Gender
+####################################################################################################
+
+## Grad Earnings
+
+#Avg DOCTORATE Grad Earnings in each Province, Year, by Gender Plot
 grad_earnings %>% filter(SCHOOL %in% c("Doctorate"), STATS == "Median", GENDER != "Both sexes", VALUE != "null") %>% ggplot(aes(x=GEO, y=VALUE, color=GENDER)) + geom_point() + ggtitle('Average Doctorate Graduate Earnings') + scale_color_brewer(palette="Set1") + facet_wrap(~REF_DATE) + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.4), plot.title = element_text(size=20, face="bold", vjust=2), axis.title.x = element_blank())
 
-#Create new table
+#Doctorate earnings table
 doctorate_earnings <- grad_earnings %>% filter(SCHOOL %in% c("Doctorate"), STATS == "Median", GENDER != "Both sexes", VALUE != "null")
 
-##Avg COLLEGE Grad Earnings in each Province, Year, by Gender
+#Avg COLLEGE Grad Earnings in each Province, Year, by Gender Plot
 grad_earnings %>% filter(SCHOOL %in% c("College"), STATS == "Median", GENDER != "Both sexes", VALUE != "null") %>% ggplot(aes(x=GEO, y=VALUE, color=GENDER)) + geom_point() + ggtitle('Average College Graduate Earnings') + scale_color_brewer(palette="Set1") + facet_wrap(~REF_DATE) + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.4), plot.title = element_text(size=20, face="bold", vjust=2), axis.title.x = element_blank())
 
-#Create new table
+#College earnings table
 college_earnings <- grad_earnings %>% filter(SCHOOL %in% c("College"), STATS == "Median", GENDER != "Both sexes", VALUE != "null")
 
-#To remove ticks and text on axis
-#axis.ticks.y = element_blank(),axis.text.y = element_blank()
+####################################################################################################
+
+##Avg 2010 Grad Debt Plot
+grad_debt %>% filter(REF_DATE == 2010, SOURCE == "Graduates who owed money for their education to government student loan programs", STATS == "Average debt owed to the source at time of graduation dollars", VALUE != "null") %>% ggplot(aes(x=GEO, y=VALUE, color=SCHOOL)) + geom_point() + ggtitle('Average Debt Owed\n to Government Loans in 2010') + scale_color_brewer(palette="Set1") + theme(axis.text.x=element_text(angle=90, size=10, vjust=0.4), axis.title.y = element_blank(), axis.title.x = element_blank(), legend.title=element_blank(), plot.title = element_text(size=20, face="bold", vjust=2, lineheight=0.8))
+
+#2010 debt table, descending by average debt
+debt2010 <- grad_debt %>% filter(REF_DATE == 2010, SOURCE == "Graduates who owed money for their education to government student loan programs", STATS == "Average debt owed to the source at time of graduation dollars", VALUE != "null") %>% arrange(desc(VALUE))
+
+####################################################################################################
+
+##Doctorate Grad Employment Plot 
+doctorate_employment_rates <- grad_employment %>% filter(SCHOOL %in% c("Doctorate"), STATS %in%  c("Unemployed rate", "Employed percent"), GENDER != "Both sexes", VALUE != "null") %>% arrange(desc(VALUE)) %>% tbl_df
+
+grad_employment %>% filter(SCHOOL %in% c("Doctorate"), STATS %in%  c("Unemployed rate", "Employed percent"), GENDER != "Both sexes", VALUE != "null") %>% arrange(desc(VALUE)) %>% ggplot(aes(x = STATS, fill = STATS)) + geom_bar(stat= "bin") + ggtitle('Doctorate Employment Across Canada') + theme(axis.title.y = element_blank(), axis.text.x = element_blank(), axis.title.x = element_blank(), plot.title = element_text(size=20, face="bold", vjust=2, lineheight=0.8), legend.title=element_blank()) + facet_wrap(~REF_DATE)
+
+##College Grad Employment Plot
+college_employment_rates <- grad_employment %>% filter(SCHOOL %in% c("College"), STATS %in%  c("Unemployed rate", "Employed percent"), GENDER != "Both sexes", VALUE != "null") %>% arrange(desc(VALUE)) %>% tbl_df
+
+grad_employment %>% filter(SCHOOL %in% c("College"), STATS %in%  c("Unemployed rate", "Employed percent"), GENDER != "Both sexes", VALUE != "null") %>% arrange(desc(VALUE)) %>% ggplot(aes(x = STATS, fill = STATS)) + geom_bar(stat= "bin") + ggtitle('College Employment Across Canada') + theme(axis.title.y = element_blank(), axis.text.x = element_blank(), axis.title.x = element_blank(), plot.title = element_text(size=20, face="bold", vjust=2, lineheight=0.8), legend.title=element_blank()) + facet_wrap(~REF_DATE)
+
+####################################################################################################
+
+##Grad Loans
+
 
