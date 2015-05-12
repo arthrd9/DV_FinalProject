9fac62ac7c9cbc4992ccafa0eff0c26aa51d293f
Madeline Packard <madelinepackard@utexas.edu>
2015-05-12 12:31:43 -0500

    fixing up RMD

--- 01 Data/Dataframes.R
+++ 01 Data/Dataframes.R
@@ -1,7 +1,6 @@
-grad_earnings <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select * from GRAD_EARNINGS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+grad_earnings <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select REF_DATE, GEO, SCHOOL, GENDER, STATS, VALUE from GRAD_EARNINGS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-grad_earnings_Edit <- select(grad_earnings, REF_DATE, GEO, SCHOOL, GENDER, STATS, VALUE) %>% tbl_df
-grad_earnings_Edit %>% select(GEO, SCHOOL, VALUE) %>% filter(SCHOOL %in% c("Doctorate")) %>% tbl_df
+grad_earnings %>% select(GEO, SCHOOL, VALUE) %>% filter(SCHOOL %in% c("Doctorate")) %>% tbl_df
 
 grad_debt <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select * from GRAD_DEBT "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
